9f2b6a64e620b54972ad63bc4273ba592a5e6ad6
stepanvanzuriak <stepanvanzuriak@gmail.com>
2018-08-31 21:50:10 +0300

    Add modular exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
-import { Finite } from './finite/finite';
-import { html } from 'lit-html/lib/lit-extended';
+import { Finite } from "./finite/finite";
+import { html } from "lit-html/lib/lit-extended";
 
 window.h = html;
-global.h = html;
 window.Finite = Finite;
-global.Finite = Finite;
+window.State = Finite.State;
+window.Render = Finite.Render;
+window.Transition = Finite.Transition;
+window.T = Finite.T;
 
+global.h = html;
+global.Finite = Finite;
+global.State = Finite.State;
+global.Render = Finite.Render;
+global.Transition = Finite.Transition;
+global.T = Finite.T;
+
+export const State = Finite.State;
+export const Render = Finite.Render;
+export const Transition = Finite.Transition;
+export const T = Finite.T;
 export { html as h };
+
 export default Finite;
