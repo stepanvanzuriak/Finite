b5f40c52e2f9ff355087f49b3caa7971cc8fd2c0
stepanvanzuriak <stepanvanzuriak@gmail.com>
2018-10-23 21:48:08 +0300

    Basic tests

--- test/basic.test.js
+++ test/basic.test.js
@@ -10,23 +10,39 @@ beforeAll(async () => {
   browser = await puppeteer.launch({
     args: ["--no-sandbox"]
   });
-});
+}, 20000);
 
 afterAll(() => {
   server.close();
   browser.close();
-});
+}, 20000);
 
-describe("Basic test", () => {
-  test(
-    "basic page",
-    async () => {
-      page = await browser.newPage();
-      await page.goto("http://localhost:8080/test.html");
+describe(
+  "Basic test",
+  async () => {
+    test(
+      "basic page",
+      async () => {
+        page = await browser.newPage();
+        await page.goto("http://localhost:8080/test.html");
 
-      let html = await page.$eval(".test", e => e.innerHTML);
-      expect(html).toBe("Hello!");
-    },
-    20000
-  );
-});
+        let html = await page.$eval(".test", e => e.innerHTML);
+        expect(html).toBe("Hello!");
+      },
+      20000
+    );
+
+    test(
+      "on click",
+      async () => {
+        page = await browser.newPage();
+        await page.goto("http://localhost:8080/onclick.html");
+        await page.click("#inc");
+        let html = await page.$eval("#count", e => e.innerHTML);
+        expect(html).toBe("1");
+      },
+      20000
+    );
+  },
+  20000
+);
