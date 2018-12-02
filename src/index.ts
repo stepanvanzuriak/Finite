7769dd7238fdd2dd391a1393cb1f8d5adcbcb78e
stepanvanzuriak <stepanvanzuriak@gmail.com>
2018-12-02 12:34:49 +0200

    Dirty version of new react hook like concept

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,65 @@
-// tslint:disable-next-line:no-submodule-imports
-import { html, raw } from "picohtml";
-import { Finite } from "./finite/finite";
+import { html, render as htmlRender, raw } from "picohtml";
 
-window.h = html;
-window.raw = raw;
-window.Finite = Finite;
-window.State = Finite.State;
-window.Render = Finite.Render;
-window.Transition = Finite.Transition;
-window.AsyncTransition = Finite.AsyncTransition;
-window.T = Finite.T;
+class Finite {
+  states = {};
+  point = null;
+  current = null;
+  currentId = null;
 
-export const State = Finite.State;
-export const Render = Finite.Render;
-export const Transition = Finite.Transition;
-export const AsyncTransition = Finite.AsyncTransition;
-export const T = Finite.T;
-export { html as h };
-export { raw };
+  render(f, point) {
+    const element = f();
 
-export default Finite;
+    this.point = point;
+    this.current = f;
+    this.currentId = element._FINITE_ID;
+    htmlRender(element, point);
+  }
+
+  rerender(id) {
+    console.log(id, this.currentId);
+    if (id === this.currentId) {
+      htmlRender(this.current(), this.point);
+    }
+  }
+}
+
+const Fin = new Finite();
+
+function defineState(memory = {}) {
+  if (!Fin.states["_id"]) {
+    Fin.states["_id"] = memory;
+  }
+
+  const set = newMemory => {
+    Fin.states["_id"] = { ...Fin.states["_id"], ...newMemory };
+    Fin.rerender("_id");
+  };
+  const wrapper = h => {
+    h._FINITE_ID = "_id";
+    return h;
+  };
+  return [Fin.states["_id"], set, wrapper];
+}
+
+function Hello() {
+  const [{ counter }, set, wrapper] = defineState({ counter: 15 });
+
+  return wrapper(html`
+    <div>
+      <button onclick="${() => set({ counter: counter + 1 })}">Hello</button>
+      <p>${counter}</p>
+    </div>
+  `);
+}
+
+// How to call subrender for Hello
+function SUP() {
+  return html`
+    <div class="app">
+      <h1>HELLO</h1>
+      ${raw(Hello())}
+    </div>
+  `;
+}
+
+Fin.render(SUP, document.body);
